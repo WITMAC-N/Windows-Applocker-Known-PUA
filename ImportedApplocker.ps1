1ad07037cad3dbbc2b3ab65a678256a58c310163
WITMAC-N <189784926+WITMAC-N@users.noreply.github.com>
2026-01-20 12:30:08 -0600

    Update ImportedApplocker.ps1
    Allow for deployment to non-provisioned devices

--- ImportedApplocker.ps1
+++ ImportedApplocker.ps1
@@ -1,5 +1,15 @@
-#this was made for my enviorment.
+# This was made for my environment.
+
 $xmlurl = "https://raw.githubusercontent.com/WITMAC-N/Windows-Applocker-Known-PUA/refs/heads/main/current.xml"
-$downloaded = "C:\Data\ProfileSetup\current.xml"
-Invoke-WebRequest $xmlurl -OutFile $downloaded
-Set-AppLockerPolicy -XMLPolicy $downloaded
+$folder = "C:\Data\ProfileSetup"
+$downloaded = "$folder\current.xml"
+
+# Ensure folder exists
+if (-not (Test-Path $folder)) {
+    New-Item -Path $folder -ItemType Directory -Force | Out-Null
+}
+
+# Download XML
+Invoke-WebRequest $xmlurl -OutFile $downloaded -ErrorAction Stop
+# Import AppLocker policy
+Set-AppLockerPolicy -XMLPolicy $downloaded -ErrorAction Stop
